3425816c4bfb9cad26db891130a3b8362b1cc144
valeria-ab <offtotheraces@inbox.ru>
2021-01-13 20:12:11 +0300

    Updates

---
+++ precache-manifest.56e86c9a42f357db0c19f9e638afa318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f16fae248d7bbfaa66d6b403fa38b4",
+    "url": "/social-network-js/index.html"
+  },
+  {
+    "revision": "130733c9a3de7474dafc",
+    "url": "/social-network-js/static/css/main.87e24d00.chunk.css"
+  },
+  {
+    "revision": "96de9cda4d7420846250",
+    "url": "/social-network-js/static/js/2.42ce078d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network-js/static/js/2.42ce078d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130733c9a3de7474dafc",
+    "url": "/social-network-js/static/js/main.e9eb4951.chunk.js"
+  },
+  {
+    "revision": "d9e5fd40f28b20cd7bae",
+    "url": "/social-network-js/static/js/runtime-main.dd8533ee.js"
+  }
+]);
